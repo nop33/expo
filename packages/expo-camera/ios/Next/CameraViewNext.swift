428a406314dacbf3cef23fd549fd5d2798e887a9
Alan Hughes <hughesno1@gmail.com>
2023-11-21 16:34:49 +0000

    [core][ios] rename function

--- packages/expo-camera/ios/Next/CameraViewNext.swift
+++ packages/expo-camera/ios/Next/CameraViewNext.swift
@@ -415,7 +415,7 @@ public class CameraViewNext: ExpoView, EXCameraInterface, EXAppLifecycleListener
 
     takenImage = ExpoCameraUtils.crop(image: takenImage, to: croppedSize)
 
-    let path = FileSystemUtilities.generatePath(
+    let path = FileSystemUtilities.generatePathInCache(
       appContext,
       in: "Camera",
       ext: ".jpg")
@@ -530,7 +530,7 @@ public class CameraViewNext: ExpoView, EXCameraInterface, EXAppLifecycleListener
           return
         }
 
-        let path = FileSystemUtilities.generatePath(self.appContext, in: "Camera", ext: ".mov")
+        let path = FileSystemUtilities.generatePathInCache(self.appContext, in: "Camera", ext: ".mov")
         let fileUrl = URL(fileURLWithPath: path)
         self.videoRecordedPromise = promise
 
